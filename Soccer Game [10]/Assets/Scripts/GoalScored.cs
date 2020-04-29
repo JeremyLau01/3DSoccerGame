3285b2ad379a85a55685611fbe96e1e6ebcf4504
Jeremy Lau <jeremylau01@gmail.com>
2020-04-29 14:39:54 -0400

    Added Folders
    Added folders to repository

---
+++ Soccer Game [10]/Assets/Scripts/GoalScored.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for text and other UI components
+using UnityEngine.Audio; //for sound effects
+
+namespace Com.JeremyLau.AvatarLeague
+{
+    public class GoalScored : MonoBehaviour
+    {
+
+
+
+        #region Variables 
+
+        public GameObject soccerBall;
+
+        public Text scoredTextBlue;
+        public Text scoredTextRed;
+        public Text scoreboardBlue;
+        public Text scoreboardRed;
+        public Text winTextBlue;
+        public Text winTextRed;
+
+
+        private AudioSource audioSource;
+        public AudioClip goalSound;
+        public AudioClip winSound;
+        public AudioClip backgroundMusic;
+
+        private int redScore = 0;
+        private int blueScore = 0;
+        // ToString()
+
+        #endregion
+
+
+        #region Monobehaviour Callbacks
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            scoredTextBlue.enabled = false;
+            scoredTextRed.enabled = false;
+
+            winTextBlue.enabled = false;
+            winTextRed.enabled = false;
+
+            scoreboardBlue.text = blueScore.ToString();
+            scoreboardRed.text = redScore.ToString();
+
+            audioSource.clip = backgroundMusic;
+            audioSource.Play();
+        }
+
+        //controls, states, and jumping in update; controls and states in FixedUpdate
+        private void Update()
+        {
+            if (redScore == 5)
+            {
+                winTextRed.enabled = true;
+                ClearScores();
+                StartCoroutine("WinRestart");
+            }
+            if (blueScore == 5)
+            {
+                winTextBlue.enabled = true;
+                ClearScores();
+                StartCoroutine("WinRestart");
+            }
+        }
+
+        public void ClearScores()
+        {
+            audioSource.clip = winSound;
+            audioSource.Play();
+            redScore = 0;
+            scoreboardRed.text = redScore.ToString();
+            blueScore = 0;
+            scoreboardBlue.text = blueScore.ToString();
+        }
+
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.tag == "TempObjTag")
+            {
+                audioSource.clip = goalSound;
+                audioSource.Play();
+                BallBackToMiddle();
+                if (this.gameObject.tag == "Blue")
+                {
+                    redScore += 1;
+                    scoreboardRed.text = redScore.ToString();
+                    scoredTextRed.enabled = true;
+                }
+                else
+                {
+                    blueScore += 1;
+                    scoreboardBlue.text = blueScore.ToString();
+                    scoredTextBlue.enabled = true;
+                }
+                StartCoroutine("WaitBeforeDestroyScoredText");
+            }
+        }
+
+        
+        public void BallBackToMiddle()
+        {
+            //Move the ball back to the middle
+            soccerBall.transform.position = new Vector3(0, 100, 0);
+
+            //Making the ball stop moving
+            soccerBall.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            soccerBall.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            soccerBall.GetComponent<Rigidbody>().Sleep();
+        }
+
+
+        void FixedUpdate()
+            {
+
+            }
+
+        IEnumerator WaitBeforeDestroyScoredText()
+        {
+            yield return new WaitForSeconds(3);
+            scoredTextBlue.enabled = false;
+            scoredTextRed.enabled = false;
+        }
+
+        IEnumerator WinRestart()
+        {
+            yield return new WaitForSeconds(6);
+            winTextBlue.enabled = false;
+            winTextRed.enabled = false;
+        }
+
+        #endregion
+    }
+}
---
+++ Soccer Game [10]/Assets/Scripts/PunchCollision.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.JeremyLau.AvatarLeague
+{
+    public class PunchCollision : MonoBehaviour
+    {
+
+
+
+        #region Variables 
+
+        public GameObject player;
+        private GameObject soccerBall;
+
+
+        #endregion
+
+
+        #region Monobehaviour Callbacks
+
+        private void Start()
+        {
+            soccerBall = GameObject.FindGameObjectWithTag("TempObjTag");
+        }
+
+        //controls, states, and jumping in update; controls and states in FixedUpdate
+        private void Update()
+        {
+
+        }
+
+
+
+
+
+        void OnTriggerStay(Collider other)
+        {
+            Vector3 aimDirection = this.transform.position - player.transform.position;
+            //aimDirection = new Vector3(aimDirection[0], aimDirection[1]*-1, aimDirection[2]);             //COMMENT OR UNCOMMENT (to change viewing angle --> shot angle)
+
+            bool punch = Input.GetKeyDown(KeyCode.Mouse0);
+            if (punch)
+            {
+                if (other.gameObject.tag == "TempObjTag") // this string is your newly created tag
+                {
+                    Debug.Log("Hitting Ball");
+                    Debug.Log(aimDirection);
+
+
+                    soccerBall.GetComponent<Rigidbody>().AddForce(aimDirection * 1000);
+                }
+                if (other.gameObject.tag == "PlayerTag")
+                {
+                    other.GetComponent<Rigidbody>().AddForce(aimDirection * 1000);
+                }
+            }
+        }
+
+
+
+
+
+
+
+        void FixedUpdate()
+        {
+
+        }
+
+        #endregion
+    }
+}
